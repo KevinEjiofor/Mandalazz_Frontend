3b4c20e3593ffa8131e641aae9d419fcf5b149db
ashleynda <ashleyndabai@gmail.com>
2025-03-23 20:52:57 +0100

    saving

---
+++ src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<script>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import ProductCard from '@/components/ProductCard.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Hero,
+    ProductCard,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <Hero />
+    <ProductCard />
+    <Footer />
+  </div>
+</template>
