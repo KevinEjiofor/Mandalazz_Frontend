85fdf11a972f0b53fa0971c667031c58451f5e81
ashleynda <ashleyndabai@gmail.com>
2025-03-23 20:58:53 +0100

    saving

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@ import Avatar from 'primevue/avatar';
 </script>
 
 <template>
-    <div class="logo flex items-center justify-between p-4 px-20 bg-white border-b-2 border-gray-200">
+    <div class="logo flex items-center justify-between p-4 px-20 bg-white divide-x divide-[#e5e7eb] fixed top-0 left-0 w-full z-50">
         <img class="h-10 w-auto" :src="logo" alt="logo" />
         <Searchbar @search="onSearch" />
         <div class="flex items-center gap-6">
